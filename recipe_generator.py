c3be7039b392ea05d2bfc06b48f8e112dca292f3
Shea Durgin <shea.durgin@alertinnovation.com>
2023-12-07 13:54:18 -0500

    requirements file and simplification

--- recipe_generator.py
+++ recipe_generator.py
@@ -1,8 +1,43 @@
 import tensorflow as tf
+from tensorflow.keras import callbacks
 from tensorflow.keras.models import load_model
-from recipe_classes import TextGenerator, TransformerBlock, TokenAndPositionEmbedding
+import numpy as np
 import json
 import tkinter as tk
+
+class TextGenerator(callbacks.Callback):
+    def __init__(self, index_to_word, model, top_k=10):
+        self.index_to_word = index_to_word
+        self.model = model
+        self.word_to_index = {
+            word: index for index, word in enumerate(index_to_word)
+        }
+
+    def sample_from(self, probs, temperature):
+        probs = probs ** (1 / temperature)
+        probs = probs / np.sum(probs)
+        return np.random.choice(len(probs), p=probs), probs
+
+    def generate(self, start_prompt, max_tokens, temperature):
+        start_tokens = [
+            self.word_to_index.get(x, 1) for x in start_prompt.split()
+        ]
+        sample_token = None
+        info = []
+        while len(start_tokens) < max_tokens and sample_token != 0:
+            x = np.array([start_tokens])
+            y, att = self.model.predict(x, verbose=0)
+            sample_token, probs = self.sample_from(y[0][-1], temperature)
+            info.append(
+                {
+                    "prompt": start_prompt,
+                    "word_probs": probs,
+                    "atts": att[0, :, -1, :],
+                }
+            )
+            start_tokens.append(sample_token)
+            start_prompt = start_prompt + " " + self.index_to_word[sample_token]
+        return start_prompt
 
 model = load_model("recipe_generator", compile=False)
 
@@ -21,7 +56,7 @@ def generate_recipe():
 root = tk.Tk()
 root.title("Recipe Generator")
 
-entry = tk.Entry(root, width=80)
+entry = tk.Entry(root, width=50)
 entry.pack()
 
 generate_button = tk.Button(root, text="Generate", command=generate_recipe)
